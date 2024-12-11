d000ec211833aa4db2a8fa4c1ab9c4c2f8195312
Pietroo03 <pietro.ponte.03@gmail.com>
2024-12-11 15:01:59 +0100

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import AppLayout from "../layout/AppLayout"
+import FilmsPage from "../pages/FilmsPage"
 
 function App() {
 
   return (
+
     <>
 
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<FilmsPage />} />
+
+          </Route>
+
+        </Routes>
+      </BrowserRouter>
+
     </>
+
   )
 }
 
