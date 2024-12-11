cb881ccc3a5b77bc211305a40ddf04d67623d97d
Pietroo03 <pietro.ponte.03@gmail.com>
2024-12-11 16:58:13 +0100

    add fetch call IT WORKS

--- components/FilmCard.jsx
+++ components/FilmCard.jsx
@@ -1,15 +1,33 @@
-export default function FilmCard({ Link, id }) {
+export default function FilmCard({ Link, id, postsData }) {
 
     return (
-        <div className="col">
-            <div className="card">
-                <img src="" className="card-img-top" alt="immagine copertina" />
-                <div className="card-body">
-                    <h5 className="card-title">Film title</h5>
-                    <p className="card-text" style={{ overflowX: 'hidden' }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore amet voluptatem consequuntur, quisquam fugit provident modi hic reprehenderit pariatur rerum.</p>
-                    <Link to={`/films/${id}`} className="btn btn-primary">Check reviews!</Link>
+
+        <section className="py-5">
+
+            <div className="container-lg">
+                <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-3">
+
+                    {postsData.movies ?
+                        postsData.movies.map((movie, index) => (
+                            <div className="col" key={index}>
+                                <div className="card">
+                                    <img src="" className="card-img-top" alt="immagine copertina" />
+                                    <div className="card-body">
+                                        <h5 className="card-title">{movie.title}</h5>
+                                        <p className="card-text" style={{ overflowX: 'hidden' }}>{movie.abstract}</p>
+                                        <Link to={`/films/${id}`} className="btn btn-primary">Check reviews!</Link>
+                                    </div>
+                                </div>
+                            </div>
+
+                        )) :
+                        <p>Nessun film</p>
+                    }
+
                 </div>
             </div>
-        </div>
+
+        </section>
+
     )
 }
--- pages/FilmsPage.jsx
+++ pages/FilmsPage.jsx
@@ -1,26 +1,35 @@
 import { Link, useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
 import FilmCard from "../components/FilmCard"
+
+const API_SERVER = import.meta.env.VITE_API_SERVER
+const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT
 
 export default function FilmsPage() {
 
+    const [postData, setPostData] = useState({})
     const { id } = useParams()
+
+    function fetchData(url = API_SERVER + API_ENDPOINT) {
+        fetch(url)
+            .then(resp => resp.json())
+            .then(data => {
+                console.log(data);
+                setPostData(data)
+            })
+            .catch(error => {
+                console.error('errore nel recupero dati', error);
+
+            })
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
 
     return (
         <>
-            <section className="py-5">
-
-                <div className="container-lg">
-                    <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-3">
-                        <FilmCard Link={Link} id={id} />
-                        <FilmCard Link={Link} id={id} />
-                        <FilmCard Link={Link} id={id} />
-                        <FilmCard Link={Link} id={id} />
-                        <FilmCard Link={Link} id={id} />
-                        <FilmCard Link={Link} id={id} />
-                    </div>
-                </div>
-
-            </section>
+            <FilmCard Link={Link} id={id} postsData={postData} />
         </>
     )
 }
