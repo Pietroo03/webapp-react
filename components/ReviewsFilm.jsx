d086b625574acd3556ebfa9f845a211c7e75f744
Pietroo03 <pietro.ponte.03@gmail.com>
2024-12-11 16:16:10 +0100

    add component with reviews of film

---
+++ components/ReviewsFilm.jsx
@@ -0,0 +1,13 @@
+export default function ReviewsFilm() {
+
+    return (
+
+        <div className="reviews card mt-3">
+            <div className="card-body">
+                <span><strong>Nome</strong></span>
+                <span> Voto</span>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit cupiditate optio voluptatem repudiandae aspernatur neque blanditiis eum! Quos, incidunt error?</p>
+            </div>
+        </div>
+    )
+}
--- pages/SingleFilm.jsx
+++ pages/SingleFilm.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom"
 import InfoFilm from "../components/InfoFilm"
+import ReviewsFilm from "../components/ReviewsFilm"
 
 export default function SingleFilm() {
 
@@ -10,15 +11,7 @@ export default function SingleFilm() {
         <section className="py-5">
             <div className="container-lg">
                 <InfoFilm />
-
-                <div className="reviews card mt-3">
-                    <div className="card-body">
-                        <span><strong>Nome</strong></span>
-                        <span> Voto</span>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit cupiditate optio voluptatem repudiandae aspernatur neque blanditiis eum! Quos, incidunt error?</p>
-                    </div>
-                </div>
-
+                <ReviewsFilm />
             </div>
         </section>
 
