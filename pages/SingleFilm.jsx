90aece0aea33a76660fc86b5cb0154a393b8ae79
Pietroo03 <pietro.ponte.03@gmail.com>
2024-12-12 11:36:58 +0100

    clean up file

--- pages/SingleFilm.jsx
+++ pages/SingleFilm.jsx
@@ -1,10 +1,6 @@
 import InfoFilm from "../components/InfoFilm"
 import ReviewsFilm from "../components/ReviewsFilm"
 
-const API_SERVER = import.meta.env.VITE_API_SERVER
-const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT
-
-
 export default function SingleFilm() {
 
     return (
