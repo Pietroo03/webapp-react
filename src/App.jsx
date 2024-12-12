cedad8995359de971b891bcc5aac45d93a517ad9
Pietroo03 <pietro.ponte.03@gmail.com>
2024-12-12 21:10:24 +0100

    import context on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,37 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import AppLayout from "../layout/AppLayout"
 import FilmsPage from "../pages/FilmsPage"
 import SingleFilm from "../pages/SingleFilm"
+import { LoadingProvider, useLoading } from "../context/LoadingContext"
+
+function Loadingmessage() {
+  const { isLoading } = useLoading()
+
+  if (!isLoading) return null
+
+  return (
+    <div className="loading-overlay">
+      <div className="loading-message">Caricamento in corso ...</div>
+    </div>
+  )
+}
 
 function App() {
 
   return (
 
     <>
-      <BrowserRouter>
-        <Routes>
+      <LoadingProvider>
+        <BrowserRouter>
+          <Routes>
 
-          <Route element={<AppLayout />}>
-            <Route index element={<FilmsPage />} />
-            <Route path="films/:id" element={<SingleFilm />} />
-          </Route>
+            <Route element={<AppLayout />}>
+              <Route index element={<FilmsPage />} />
+              <Route path="films/:id" element={<SingleFilm />} />
+            </Route>
 
-        </Routes>
-      </BrowserRouter>
+          </Routes>
+        </BrowserRouter>
+      </LoadingProvider>
 
     </>
 
