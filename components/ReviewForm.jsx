e3d6b8f0a2b0553c58f3ca101f6fa9d41a0ce055
Pietroo03 <pietro.ponte.03@gmail.com>
2024-12-13 16:59:23 +0100

    add fetch call

--- components/ReviewForm.jsx
+++ components/ReviewForm.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react"
 
-export default function ReviewForm() {
+const API_SERVER = import.meta.env.VITE_API_SERVER
+const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT
+
+export default function ReviewForm({ movie_id }) {
 
     const [name, setName] = useState('')
     const [review, setReview] = useState('')
@@ -18,6 +21,20 @@ export default function ReviewForm() {
 
         console.log(formData);
 
+        fetch(`${API_SERVER}${API_ENDPOINT}/${movie_id}/review`, {
+            method: 'POST',
+            body: JSON.stringify(formData),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
+            .catch(err => console.log(err)
+            )
+
     }
 
     return (
@@ -30,8 +47,12 @@ export default function ReviewForm() {
                         <label htmlFor="name">Username</label>
                         <input name="name" id="name" type="text" className="form-control" placeholder="Inserisci l'username" value={name} onChange={(e) => setName(e.target.value)} />
 
-                        <label htmlFor="text">Review</label>
-                        <input name="text" id="text" type="text" className="form-control" placeholder="Inserisci la tua recensione" value={review} onChange={(e) => setReview(e.target.value)} />
+                        <div className="rating my-3 text-warning">
+                            {[1, 2, 3, 4, 5].map(n => <i key={n} className={`bi bi-star${n <= vote ? '-fill' : ''} `} onClick={() => setVote(n)}></i>)}
+                        </div>
+
+                        <label htmlFor="review">Review</label>
+                        <input name="review" id="text" type="text" className="form-control" placeholder="Inserisci la tua recensione" value={review} onChange={(e) => setReview(e.target.value)} />
 
                         <button type="submit" className="btn btn-dark">Invia recensione</button>
 
