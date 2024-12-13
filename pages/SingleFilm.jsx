2b9095730f5409a8d54717d59c88ad0af5b62456
Pietroo03 <pietro.ponte.03@gmail.com>
2024-12-13 16:59:49 +0100

    add params

--- pages/SingleFilm.jsx
+++ pages/SingleFilm.jsx
@@ -1,14 +1,17 @@
+import { useParams } from "react-router-dom"
 import InfoFilm from "../components/InfoFilm"
 import ReviewsFilm from "../components/ReviewsFilm"
 import ReviewForm from "../components/ReviewForm"
 
 export default function SingleFilm() {
 
+    const { id } = useParams()
+
     return (
 
         <section className="py-5">
             <div className="container-lg">
-                <ReviewForm />
+                <ReviewForm movie_id={id} />
                 <InfoFilm />
                 <ReviewsFilm />
             </div>
