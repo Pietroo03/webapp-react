f8f2485c7852021efc89d82132b149e69bf0c031
Pietroo03 <pietro.ponte.03@gmail.com>
2024-12-16 17:22:27 +0100

    clean up code

--- src/context/LoadingContext.jsx
+++ src/context/LoadingContext.jsx
@@ -1,23 +1,3 @@
-/* import { createContext, useContext, useState } from "react";
-
-const LoadingContext = createContext()
-
-export const LoadingProvider = ({ children }) => {
-    const [isLoading, setIsLoading] = useState(false)
-
-    const startLoading = () => setIsLoading(true)
-    const stopLoading = () => setIsLoading(false)
-
-    return (
-        <LoadingContext.Provider value={{ isLoading, startLoading, stopLoading }} >
-            {children}
-        </LoadingContext.Provider>
-    )
-
-}
-
-export const useLoading = () => useContext(LoadingContext) */
-
 import { createContext } from "react";
 
 const LoadingContext = createContext()
