fd17c01d215a0064403ee7832acb013b5199b995
Pietroo03 <pietro.ponte.03@gmail.com>
2024-12-16 17:22:55 +0100

    clean up code

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,6 @@ import FilmsPage from "./pages/FilmsPage"
 import SingleFilm from "./pages/SingleFilm"
 import LoadingContext from "./context/LoadingContext"
 import { useState } from "react"
-//import { LoadingProvider, useLoading } from "./context/LoadingContext"
-
-/* function Loadingmessage() {
-  const { isLoading } = useLoading()
-
-  if (!isLoading) return null
-
-  return (
-    <div className="loading-overlay position-fixed top-0 left-0 w-100 h-100 bg-light d-flex justify-content-center align-items-center">
-      <div className="loading-message fs-3">Caricamento in corso ...</div>
-    </div>
-  )
-} */
 
 function App() {
 
@@ -30,7 +17,6 @@ function App() {
   return (
 
     <>
-
       <LoadingContext.Provider value={values}>
         <BrowserRouter>
           <Routes>
@@ -43,7 +29,6 @@ function App() {
           </Routes>
         </BrowserRouter>
       </LoadingContext.Provider>
-
     </>
 
   )
